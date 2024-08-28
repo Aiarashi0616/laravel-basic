2116f2eaf334d6230df023454a050b2234379981
Aiarashi0616 <morningaaa0616@gmail.com>
2024-08-28 11:12:17 +0900

    20章を追加する

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\VendorController;
+use App\Http\Controllers\RequestController;
 
-Route::get('/hello', [HelloController::class, 'index']);
 
 /*
 |--------------------------------------------------------------------------
@@ -21,9 +23,17 @@ Route::get('/hello', [HelloController::class, 'index']);
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/hello', function () {
+    return 'Hello, world!';
+});
+
+ Route::get('/hello', [HelloController::class, 'index']);
  Route::get('/posts', [HelloController::class, 'index']);
  Route::get('/products/{id}', [ProductController::class, 'show']);
  Route::get('/vendors/{id}', [VendorController::class, 'show']);
+ Route::get('/requests/create', [RequestController::class, 'create']);
+ 
+ Route::post('/requests/confirm', [RequestController::class, 'confirm'])->name('requests.confirm');
 
 
 
